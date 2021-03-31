8ea28c1715b0a8b35a52dfdb3661525fdb5c98d3
Ankush Kuwar <33517094+Anku5hk@users.noreply.github.com>
2021-03-31 14:51:53 +0530

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -139,7 +139,7 @@ class QGPipeline:
                 
                 answer_text = answer_text.strip()
                 
-                ans_start_idx = sent.index(answer_text)
+                ans_start_idx = sent.startswith(answer_text)
                 
                 sent = f"{sent[:ans_start_idx]} <hl> {answer_text} <hl> {sent[ans_start_idx + len(answer_text): ]}"
                 sents_copy[i] = sent
